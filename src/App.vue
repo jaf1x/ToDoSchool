ae2a64e7d9f88eb24bf11b3e4e43dc95e2fb9cc2
jaf1x <zepy27012010@gmail.com>
2024-02-22 23:44:37 +0300

    12

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Theheader from './/components/TheHeader.vue'
+import TheNav from './components/TheNav.vue'
+</script>
+
+<template>
+  <main class="absolute mt-24 ml-24 flex">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod aliquid dolores tempore
+    perferendis doloribus, eum blanditiis et unde optio? Voluptas ad corrupti porro quia ex dolor,
+    harum blanditiis ab sunt qui illo perspiciatis saepe laborum delectus laudantium quasi sint,
+    odio beatae ea voluptatibus voluptate? Id, cumque incidunt fugit, sed commodi nemo velit esse
+    laboriosam unde omnis reiciendis placeat. Necessitatibus hic sunt vitae, omnis deserunt nisi.
+    Ipsum doloribus iste numquam quam nulla suscipit obcaecati fugit autem corporis deleniti soluta
+    eos dolorem saepe quia totam ipsam, sed accusantium vel perferendis tempora! Quam, aliquid
+    deleniti. Nesciunt architecto expedita modi assumenda facilis placeat quam.
+  </main>
+  <Theheader />
+  <TheNav class="" />
+</template>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+</script>
+<template>
+  <nav class="sticky right-0 z-10">
+    <ul class="menu p-4 overflow-y-auto w-20 bg-base-100 border-r">
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 capitalize h-20" href="#timeline">
+          <ClockIcon class="h-6 w-6" /> timeline
+        </a>
+      </li>
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 capitalize h-20" href="#activities">
+          <ListBulletIcon class="h-6 w-6" /> activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 capitalize h-20" href="#progress">
+          <ChartBarIcon class="h-6 w-6" /> progress
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
